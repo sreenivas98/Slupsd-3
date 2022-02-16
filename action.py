78302abc621ebe8016550e7de6cdbdbaaf5be921
Sai Sreenivas Reddy Karra <42874557+sreenivas98@users.noreply.github.com>
2022-02-16 11:36:08 -0600

    Create action.py
    Executes an action and checks for its success

---
+++ action.py
@@ -0,0 +1,16 @@
+def check_for_success(command):
+    while True:
+        received = input()
+        if received == 'succeeded' + command:
+            return True
+        elif received.startswith('failed' + command):
+            return False
+        elif received.startswith('error' + command):
+            return False
+            
+def action(command, wait=True):
+    print('start' + command , flush=True)
+    if wait==True:
+        return check_for_success(command)
+    else:
+        return True
