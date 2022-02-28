6c4f5c48cd2724e66926ab75b661b3ba00463b94
Sai Sreenivas Reddy Karra <srivatsava.sai@gmail.com>
2022-02-28 10:11:45 -0600

    P3-31 Create Items

--- src/Create_Item.py
+++ src/Create_Item.py
@@ -1,4 +1,5 @@
 from action import action
+#class item used for creating and setting position of an item
 class item:
     def __init__(self,item_name,item,position):
         self.item_name=item_name
