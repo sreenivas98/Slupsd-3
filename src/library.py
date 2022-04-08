f53f777b4d5a733ec911ac654be3ef6eb64fd87c
Sai hasmitha Goli <99351711+hasmitha111@users.noreply.github.com>
2022-04-08 10:00:18 -0500

    code for library

---
+++ src/library.py
@@ -0,0 +1,13 @@
+from Location import *
+from CreateCharacter import *
+from action import action
+
+
+def placeLibrary():
+    action('EnableIcon("Talk",talk,'+Librarian.name+',"Talk about Palpatine",true)')
+    # action('EnableIcon("Pick",draw,'+Scroll.name+',"Pick up scroll",true)')
+    action('FadeIn()')
+    action('Enter('+Harry.name+','+Library.name+'.Door)')
+    action('SetCameraFocus('+Harry.name+')')
+    action('SetCameraMode(follow)')
+    action('EnableInput()')
