39ee8f4198094844f1799873187cbeeabace3efe
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2022-04-08 10:25:42 -0500

    Update CourtYard2.py

---
+++ src/CourtYard2.py
@@ -0,0 +1,12 @@
+from Locations import *
+from CreateCharacters import *
+from action import action
+
+def CourtYard():
+    action('EnableIcon("Exit",exit,'+Moore_Courtyard.name+'.Exit,"Exit Court Yard",true)')
+    action('EnableIcon("Talk",talk,'+CYWorker.name+',"Talk about Kidnapping",true)')
+    action('FadeIn()')
+    action('Enter('+Harry.name+','+Moore_Courtyard.name+'.Gate)')
+    action('SetCameraFocus('+Harry.name+')')
+    action('SetCameraMode(follow)')
+    action('EnableInput()')
