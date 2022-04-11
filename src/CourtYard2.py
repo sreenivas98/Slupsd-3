b5c401568dcb956e236f8a80cd099a9b6da3cb1a
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2022-04-11 10:23:18 -0500

    Update CourtYard2.py

--- src/CourtYard2.py
+++ src/CourtYard2.py
@@ -3,7 +3,7 @@ from CreateCharacters import *
 from action import action
 
 def CourtYard():
-    action('EnableIcon("Exit",exit,'+Moore_Courtyard.name+'.Exit,"Exit Court Yard",true)')
+    #action('EnableIcon("Exit",exit,'+Moore_Courtyard.name+'.Exit,"Exit Court Yard",true)')
     action('EnableIcon("Talk",talk,'+CYWorker.name+',"Talk about Kidnapping",true)')
     action('FadeIn()')
     action('Enter('+Harry.name+','+Moore_Courtyard.name+'.Gate)')
